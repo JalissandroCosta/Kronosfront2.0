5c85c484c932dd7cf146d1c94ef575ad68541260
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-04-10 11:55:32 -0400

    :sparkles: feat: add DataTable

--- apps/web/app/dashboard/list-prisioners/page.tsx
+++ apps/web/app/dashboard/list-prisioners/page.tsx
@@ -7,7 +7,7 @@ export default async function PagePrisioner() {
   return (
     <section className="w-full flex flex-col items-center justify-center p-4 gap-3 ">
        <h2 className="font-bold text-2xl uppercase">Lista de Prisioneiros</h2>
-       <DataTable columns={columns} data={allPrisioners} />
+       <DataTable columns={columns} data={allPrisioners} search={"nome"}/>
    </section>
 
   );
