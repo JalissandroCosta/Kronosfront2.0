72af8859fa6e4b54bc10ee432bcb418889ef23a1
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-04-10 11:58:17 -0400

    :wrench: chore:  allowing urls

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  images: {
+   remotePatterns:[
+    {
+      hostname:"cdn.jsdelivr.net",
+      protocol:"https",
+      pathname:"/**",
+    },
+    {
+      hostname:"avatars.githubusercontent.com",
+      protocol:"https",
+      pathname:"/**",
+    },
+    {
+      hostname:"tvhistoria.com.br",
+      protocol:"https",
+      pathname:"/**",
+    }
+   ]
+  },
 }
 
 export default nextConfig
