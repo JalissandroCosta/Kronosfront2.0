c020223866e93b87d15fe0fe53a6cd6609b66ecd
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-04-14 09:32:09 -0400

    :sparkles: feat: Add imput personalizado com react-hook-form

---
+++ packages/ui/src/components/fields/field.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps } from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
+import { Input } from '../input.js'
+import { FieldWrapper } from './field-wrapper.js'
+
+
+type InputField = ComponentProps<typeof Input> & {
+  label: string
+  name: string
+}
+
+export const InputField = ({ label, name, required, ...props }: InputField) => {
+  const { control } = useFormContext()
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{
+        required: required && 'Campo Obrigatório'
+      }}
+      render={({ field, fieldState }) => (
+        <FieldWrapper label={label} {...props}>
+          <Input {...field} {...props} />
+          {fieldState.error && (
+            <p className="text-sm text-red-500">{fieldState.error.message}</p>
+          )}
+        </FieldWrapper>
+      )}
+    />
+  )
+}
