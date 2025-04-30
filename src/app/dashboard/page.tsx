60366f450f9c694c00054eeea05f097473abc5da
Jalissandro Costa <Jalissandro.barros345@gmail.com>
2025-04-30 15:42:48 -0400

    style: adicionando estilo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { ChartAreaInteractive } from '@/components/chart-area-interactive'
 import { DataTable } from '@/components/data-table'
+import { BarChartComponet, PieChartComponet } from '@/components/bar-chart'
 import { SectionCards } from '@/components/section-cards'
 
 export default function Page() {
@@ -9,7 +10,7 @@ export default function Page() {
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <SectionCards />
           <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
+           <BarChartComponet />
           </div>
           {/* <DataTable data={data} /> */}
         </div>
