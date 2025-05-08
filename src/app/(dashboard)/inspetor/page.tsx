c6d35e5e19bf7b0cdb73b549fcbeca7cb4025dc7
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-05-08 14:55:12 -0400

    :construction: Pagina principal de INPETOR
    Ainda se decidindo o que terá aqui

--- src/app/(dashboard)/inspetor/page.tsx
+++ src/app/(dashboard)/inspetor/page.tsx
@@ -1,37 +1,26 @@
-import { getAllPrisioners } from '@/actions/prisioner'
-import { getAllVisitas } from '@/actions/visitas'
-import { ChartBar } from '@/components/chart-bar'
-import { SectionCards } from '@/components/section-cards'
-import {
-  contarPrisioneirosPorMes,
-  contarVisitasPorMes,
-  getTop6DetentosMaisVisitados,
-  mesclarDados
-} from '@/utils/functions'
-import { BarCharVisitarPresoComponent } from '../_componets/bar-char-vistas-por-preso'
 
 
 export default async function Page() {
-  const [prisioneiros, visitas] = await Promise.all([
-    getAllPrisioners(),
-    getAllVisitas()
-  ])
+  // const [prisioneiros, visitas] = await Promise.all([
+  //   getAllPrisioners(),
+  //   getAllVisitas()
+  // ])
 
-  const top6MaisVisitados = getTop6DetentosMaisVisitados(visitas)
+  // const top6MaisVisitados = getTop6DetentosMaisVisitados(visitas)
 
-  const visitasCount = contarVisitasPorMes(visitas)
-  const prisioneirosCount = contarPrisioneirosPorMes(prisioneiros)
+  // const visitasCount = contarVisitasPorMes(visitas)
+  // const prisioneirosCount = contarPrisioneirosPorMes(prisioneiros)
 
-  const dadosMesclados = mesclarDados(visitasCount, prisioneirosCount)
+  // const dadosMesclados = mesclarDados(visitasCount, prisioneirosCount)
 
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <SectionCards data={dadosMesclados} />
+          {/* <SectionCards data={dadosMesclados} /> */}
           <div className="w-full gap-6 justify-evenly flex flex-col lg:flex-row px-4 lg:px-5">
-            <ChartBar chartData={dadosMesclados} />
-            <BarCharVisitarPresoComponent chartData={top6MaisVisitados}/>
+            {/* <ChartBar chartData={dadosMesclados} /> */}
+            {/* <BarCharVisitarPresoComponent chartData={top6MaisVisitados}/> */}
           </div>
         </div>
       </div>
