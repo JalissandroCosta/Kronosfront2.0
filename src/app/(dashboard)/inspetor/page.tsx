b4f4f043bad5120bd5aa766223e4d8a79668889f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-05-28 09:07:17 -0400

    feat: clean up unused code and comments in Inspetor page component

--- src/app/(dashboard)/inspetor/page.tsx
+++ src/app/(dashboard)/inspetor/page.tsx
@@ -1,24 +1,19 @@
 export default async function Page() {
-  // const [prisioneiros, visitas] = await Promise.all([
-  //   getAllPrisioners(),
-  //   getAllVisitas()
-  // ])
 
-  // const top6MaisVisitados = getTop6DetentosMaisVisitados(visitas)
-
-  // const visitasCount = contarVisitasPorMes(visitas)
-  // const prisioneirosCount = contarPrisioneirosPorMes(prisioneiros)
-
-  // const dadosMesclados = mesclarDados(visitasCount, prisioneirosCount)
 
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          {/* <SectionCards data={dadosMesclados} /> */}
+         
           <div className="flex w-full flex-col justify-evenly gap-6 px-4 lg:flex-row lg:px-5">
-            {/* <ChartBar chartData={dadosMesclados} /> */}
-            {/* <BarCharVisitarPresoComponent chartData={top6MaisVisitados}/> */}
+          <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6 leading-tight">
+            Bem vindo Inspetor{" "}!
+            <br/>
+            <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+              Sua nova realidade comeca aqui!
+            </span>
+          </h1>
           </div>
         </div>
       </div>
